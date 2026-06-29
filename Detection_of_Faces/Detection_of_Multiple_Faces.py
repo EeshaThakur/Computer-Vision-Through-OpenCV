b20571091c75c3ad80540d784a4066ce73eff84d
EeshaThakur <eeshathakur91@gmail.com>
2026-06-30 01:52:18 +0530

    Multi-Face Detection

---
+++ Detection_of_Faces/Detection_of_Multiple_Faces.py
@@ -0,0 +1,29 @@
+import cv2
+
+img = cv2.imread("multiple_faces.jpg")
+
+if img is None:
+    print("Image not found!")
+    exit()
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+
+faces = face_cascade.detectMultiScale(
+    gray,
+    scaleFactor=1.05,
+    minNeighbors=6,
+    minSize=(50, 50)
+)
+
+print("Faces detected:", len(faces))
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+cv2.imshow("Face Detection", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
